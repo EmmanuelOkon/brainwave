f1e985f0ca9e921389acbd071333996ff1666148
Emmanuel Okon <emmaudo27@gmail.com>
2024-03-12 11:45:15 +0100

    created reusable custom button, header component, design files with custom background and custom hamburger with transition styles

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src", // Resolves '@' alias to the 'src' directory
+    },
+  },
+  // server: {
+  //   hmr: {
+  //     overlay: false, // Disable HMR overlay
+  //   },
+  // },
+});
